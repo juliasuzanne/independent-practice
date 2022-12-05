fac10f0f8fc57b4ab61c5149ef87ba9f732c3ffc
Julia Grimes <jgrimes@cfs.energy>
2022-12-05 10:33:19 -0500

    Working on isbn exercise, need to verify that the X character in center will always be caught whether or not I include the commented ? part

--- w01/03_isbn/isbn.rb
+++ w01/03_isbn/isbn.rb
@@ -1,3 +1,51 @@
 def isbn_verify?(isbn_string)
+  # remove any dashes
+  isbn = isbn_string.gsub('-', '')
+  isbn = isbn.gsub(/[a-wy-zA-WY-Z]/, "11")
+  # get length of string
+  isbn_length = isbn.length
+  # split string into array
+  isbn = isbn.chars()
+  # first verify length to move onto algorithm
+  if isbn_length == 10
+
+    # interpret X at end as 10
+    if isbn[isbn.length - 1] == "X"
+      isbn[isbn.length - 1] = "10"
+    end
+
+    p isbn
+
+    # if there is still an X, it is not at the end, reject (?? still worked without this)
+    isbn.each do |x|
+      if x == "X" || x == "x"
+        return false
+        break
+      end
+    end
+
+    p isbn
+    # loop through each digit and multiply according to ISBN rules
+    i = 0
+    sum = 0
+    while i < 10 do
+      sum = sum + (isbn[i].to_i * (10 - i))
+      i+=1
+    end
+
+    # check if sum is divisible by 11, if so it is a true ISBN #
+    if sum % 11 == 0
+      return true
+    else
+      return false
+    end
+
+  # if isbn length is not 10
+  else
+    return false
+  end
+
   # Your job is to write the code for this method!
 end
+
+puts isbn_verify?("3-598-X5081X-")
